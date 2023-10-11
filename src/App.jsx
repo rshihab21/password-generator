f4a0f0d246bd64cf94278ccc94435f1bb8b0d4bd
rshihab21 <shihabcse21@gmail.com>
2023-10-11 23:04:29 +0600

    Design UI for password generator application by tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 function App() {
   return (
-    <>
-      <h1>Password Generator</h1>
-    </>
+    <div className="text-white bg-slate-400 py-3 px-4 mx-auto max-w-md mt-3 rounded">
+      <h1 className="text-center text-2xl text-slate-800">
+        Password Generator
+      </h1>
+      <div>
+        <div className="flex rounded-lg overflow-hidden mt-2">
+          <input
+            type="text"
+            readOnly
+            className="w-full text-black px-1 outline-none"
+          />
+          <button className="bg-orange-700 py-2 px-3">Copy</button>
+        </div>
+        <div className="flex px-2 mt-2 align-center">
+          <div className="flex align-center px-2">
+            <input type="range" name="" id="" />
+            <label htmlFor="length">Length</label>
+          </div>
+          <div className="flex align-center px-2">
+            <input type="checkbox" name="" id="" />
+            <label htmlFor="number">Number</label>
+          </div>
+          <div className="flex align-center px-2">
+            <input type="checkbox" name="" id="" />
+            <label htmlFor="character">character</label>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
